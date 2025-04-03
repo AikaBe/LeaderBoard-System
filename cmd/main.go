d7fec1a24f95734b90e9d822108a8c9f9b207123
abeiseke <student@ALEM-F4-R05-06.alem.school>
2025-04-03 15:26:24 +0500

    cookies

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
-package cmd
+package main
+
+import (
+	"1337b04rd/internal/interface/handlers"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/setcookie", handlers.SetCookie)
+	http.HandleFunc("/getcookie", handlers.GetCookie)
+
+	fmt.Println("Server start at http://localhost:8080 ")
+	http.ListenAndServe(":8080", nil)
+}
