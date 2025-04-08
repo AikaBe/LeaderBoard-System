3d6b19535c0267fa179235b43cfb72ffd9f8bca8
lalpieva <student@ALEM-F4-R05-06.alem.school>
2025-04-08 19:51:31 +0500

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -13,3 +13,32 @@ func main() {
 	fmt.Println("Server start at http://localhost:8080 ")
 	http.ListenAndServe(":8080", nil)
 }
+
+// // Строка подключения к PostgreSQL
+// connStr := "postgres://user:password@localhost:5432/mydb?sslmode=disable"
+// db, err := sql.Open("postgres", connStr)
+// if err != nil {
+// 	log.Fatal("Unable to connect to database:", err)
+// }
+// defer db.Close()
+
+// // Проверка подключения
+// err = db.Ping()
+// if err != nil {
+// 	log.Fatal("Unable to ping database:", err)
+// }
+
+// // Создаем репозиторий
+// postRepo := repositories.NewPostRepositoryPg(db)
+
+// // Создаем сервис с репозиторием
+// postService := services.NewPostService(postRepo)
+
+// // Пример работы с сервисом
+// _, err = postService.CreatePost("Post title", "Post text", "user123", "User Name", "User Avatar", "http://example.com/image.jpg")
+// if err != nil {
+// 	log.Fatal("Error creating post:", err)
+// }
+
+// fmt.Println("Post created successfully!")
+// }
