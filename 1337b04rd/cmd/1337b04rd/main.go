825efb1befbc556c9f5605fab9603f5108e538a2
lalpieva <Alpieva60023081@gmail.com>
2025-05-01 17:35:17 +0500

    lyalya

--- 1337b04rd/cmd/1337b04rd/main.go
+++ 1337b04rd/cmd/1337b04rd/main.go
@@ -1,61 +1,108 @@
 package main
 
 import (
-	"1337b04rd/internal/interface/middleware"
+	d "1337b04rd/internal/adapters/database"
+	"1337b04rd/internal/app/domain/ports"
+	"1337b04rd/internal/app/domain/services"
+	"1337b04rd/internal/interface/handlers"
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
 
-	_ "github.com/lib/pq"
+	_ "github.com/lib/pq" // Импортируем драйвер PostgreSQL
 )
 
-func main() {
-	// Database connection string
-	connStr := "host=db port=5432 user=board_user password=board_pass dbname=board_db sslmode=disable"
-
-	// Подключение к базе данных
-	db, err := sql.Open("postgres", connStr)
+func initRepository(dsn string) (ports.PostRepository, ports.CommentRepository, *sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		log.Fatal("Database connection error: ", err)
+		return nil, nil, nil, fmt.Errorf("error connecting to database: %v", err)
+	}
+
+	postRepo := d.NewPostRepositoryPg(db)
+	commentRepo := d.NewCommentRepositoryPg(db)
+
+	return postRepo, commentRepo, db, nil
+}
+
+func main() {
+	// DSN строка подключения
+	dsn := "host=db port=5432 user=latte password=latte dbname=frappuccino sslmode=disable"
+
+	postRepo, commentRepo, db, err := initRepository(dsn)
+	if err != nil {
+		log.Fatalf("Ошибка инициализации репозитория: %v", err)
 	}
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
-		log.Fatal("Error trying to connect to database: ", err)
-	}
+	// Сервисы
+	postService := services.NewPostService(postRepo, nil)
+	commentService := services.NewCommentService(commentRepo)
+
+	// Хэндлеры
+	postHandler := handlers.NewPostHandler(postService)
+	commentHandler := handlers.NewCommentHandler(commentService)
 
 	// HTTP роутинг
 	mux := http.NewServeMux()
 
-	// Обработка запросов на статичные файлы (HTML, CSS, JS)
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/templates"))))
+	// Роуты для постов
+	mux.HandleFunc("/posts", postHandler.GetAllPosts)      // Получение всех постов
+	mux.HandleFunc("/create-post", postHandler.CreatePost) // Создание нового поста
 
-	// Главный маршрут, который будет открывать файл catalog.html
-	mux.Handle("/", middleware.LoginOrLastVisitHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("web", "templates", "catalog.html"))
-	})))
-	// mux.HandleFunc("/chat", middleware.LoginOrLastVisitHandler)
-	mux.Handle("/create-post", middleware.LoginOrLastVisitHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("web", "templates", "create-post.html"))
-	})))
-	mux.Handle("/archive", middleware.LoginOrLastVisitHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("web", "templates", "archive.html"))
-	})))
-	mux.Handle("/archive-post", middleware.LoginOrLastVisitHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("web", "templates", "archive-post.html"))
-	})))
+	// Роуты для комментариев
+	mux.HandleFunc("/comments", commentHandler.CreateComment) // Создание нового комментария
 
-	mux.Handle("/post", middleware.LoginOrLastVisitHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("web", "templates", "post.html"))
-	})))
-	mux.Handle("/error", middleware.LoginOrLastVisitHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join("web", "templates", "error.html"))
-	})))
-
-	log.Println("The server is running on port 8080...")
+	// Запуск сервера
+	log.Println("Сервер работает на порту 8080...")
 	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
-		log.Fatal("Error starting server: ", err)
+		log.Fatal("Ошибка при запуске сервера: ", err)
 	}
 }
+
+// package main
+
+// import (
+// 	"1337b04rd/internal/interface/middleware"
+// 	"database/sql"
+// 	"log"
+// 	"net/http"
+// 	"path/filepath"
+
+// 	_ "github.com/lib/pq" // Импортируем драйвер PostgreSQL
+// )
+
+// func main() {
+// 	// Database connection string
+// 	connStr := "host=db port=5432 user=latte password=latte dbname=frappuccino sslmode=disable"
+// 	// Open a connection to the database
+// 	db, err := sql.Open("postgres", connStr)
+// 	if err != nil {
+// 		log.Fatal("Error connecting to the database: ", err)
+// 	}
+// 	defer db.Close()
+
+// 	if err := db.Ping(); err != nil {
+// 		log.Fatal("Error pinging the database: ", err)
+// 	}
+
+// 	// Прокидываем зависимости в middleware
+// 	mux := http.NewServeMux()
+
+// 	// Обработка запросов на статичные файлы (HTML, CSS, JS)
+// 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/templates"))))
+
+// 	// Главный маршрут, который будет открывать файл catalog.html
+// 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		http.ServeFile(w, r, filepath.Join("web", "templates", "catalog.html"))
+// 	})
+// 	mux.HandleFunc("/chat", middleware.LoginOrLastVisitHandler)
+
+// 	// Запуск сервера
+// 	log.Println("Сервер работает на порту 8080...")
+// 	err = http.ListenAndServe(":8080", mux)
+// 	if err != nil {
+// 		log.Fatal("Ошибка при запуске сервера: ", err)
+// 	}
+// }
