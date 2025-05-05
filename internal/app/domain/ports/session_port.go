d8f533af4cb345ad3dc4a332593ed9208c45ec54
abeiseke <abeiseke@noreply.platform.alem.school>
2025-05-05 07:11:29 +0000

    Delete internal/app/domain/ports/session_port.go

--- internal/app/domain/ports/session_port.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "1337b04rd/internal/app/domain/models"
-
-type SessionRepository interface {
-	GetSessionData(sessionID string) (models.UserData, bool)
-	SetSessionData(sessionID string, data models.UserData) error
-}
