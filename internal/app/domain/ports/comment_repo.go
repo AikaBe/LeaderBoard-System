28b71f657480a82b6e60736fbb5fa331b1fe2fb2
abeiseke <abeiseke@noreply.platform.alem.school>
2025-05-05 07:11:05 +0000

    Delete internal/app/domain/ports/comment_repo.go

--- internal/app/domain/ports/comment_repo.go
+++
@@ -1,9 +0,0 @@
-package ports
-
-import "1337b04rd/internal/app/domain/models"
-
-type CommentRepository interface {
-	CreateComment(comment models.Comment) (*models.Comment, error)
-	GetCommentsByPostID(postID int) ([]*models.Comment, error)
-	DeleteComment(commentID string) error
-}
